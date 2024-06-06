e04db583c836103d28667718ab609e3220ec0c15
nomutin <nomura0508@icloud.com>
2024-06-06 21:49:16 +0900

    [add] テストケース

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-# ruff: noqa: PLR6301
 """テストの共通設定."""
 
 import pytest
@@ -7,10 +6,10 @@ from torch import Tensor, nn
 
 BATCH_SIZE, SEQ_LEN = 4, 8
 CHANNELS, HEIGHT, WIDTH = 3, 64, 64
-ACTION_SIZE, EMBED_DIM = 4, 16
+ACTION_DIM, STATE_DIM, EMBED_DIM = 4, 16, 50
 
 
-class DummyEncoder(nn.Module):
+class DummyObsEncoder(nn.Module):
     """
     ダミーの観測のエンコーダ.
 
@@ -20,6 +19,19 @@ class DummyEncoder(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         """入力に関係なく適切な形状のテンソルを返す."""
         batch_size, _, _, _ = x.shape
+        return torch.rand(batch_size, EMBED_DIM)
+
+
+class DummyStateEncoder(nn.Module):
+    """
+    ダミーの状態のエンコーダ.
+
+    [B, S] -> [B, E] へ変換するモデル.
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        """入力に関係なく適切な形状のテンソルを返す."""
+        batch_size, _, = x.shape
         return torch.rand(batch_size, EMBED_DIM)
 
 
@@ -38,23 +50,35 @@ class DummyHead(nn.Module):
 
 @pytest.fixture()
 def observations() -> Tensor:
-    """4次元の観測."""
+    """観測."""
     return torch.rand(BATCH_SIZE, CHANNELS, HEIGHT, WIDTH)
 
 
 @pytest.fixture()
 def observations_timeseries() -> Tensor:
-    """5次元の観測."""
+    """観測時系列."""
     return torch.rand(BATCH_SIZE, SEQ_LEN, CHANNELS, HEIGHT, WIDTH)
 
 
 @pytest.fixture()
 def actions() -> Tensor:
-    """2次元の行動."""
-    return torch.rand(BATCH_SIZE, ACTION_SIZE)
+    """行動."""
+    return torch.rand(BATCH_SIZE, ACTION_DIM)
 
 
 @pytest.fixture()
 def actions_timeseries() -> Tensor:
-    """3次元の行動."""
-    return torch.rand(BATCH_SIZE, SEQ_LEN, ACTION_SIZE)
+    """行動時系列."""
+    return torch.rand(BATCH_SIZE, SEQ_LEN, ACTION_DIM)
+
+
+@pytest.fixture()
+def states() -> Tensor:
+    """状態."""
+    return torch.rand(BATCH_SIZE, STATE_DIM)
+
+
+@pytest.fixture()
+def states_timeseries() -> Tensor:
+    """状態時系列."""
+    return torch.rand(BATCH_SIZE, SEQ_LEN, STATE_DIM)
--- tests/test__core.py
+++ tests/test__core.py
@@ -5,21 +5,35 @@ from torch import Tensor
 
 from ibc_lightning import IBC
 from tests.conftest import (
-    ACTION_SIZE,
+    ACTION_DIM,
     BATCH_SIZE,
-    DummyEncoder,
+    DummyObsEncoder,
+    DummyStateEncoder,
     DummyHead,
 )
 
 
 @pytest.fixture()
 def ibc() -> IBC:
-    """ダミーの IBC インスタンス."""
+    """ダミーの IBC インスタンス(行動-観測)."""
     return IBC(
-        obs_encoder=DummyEncoder(),
+        state_encoder=DummyObsEncoder(),
         energy_head=DummyHead(),
-        upper_bounds=tuple(1.0 for _ in range(ACTION_SIZE)),
-        lower_bounds=tuple(-1.0 for _ in range(ACTION_SIZE)),
+        upper_bounds=tuple(1.0 for _ in range(ACTION_DIM)),
+        lower_bounds=tuple(-1.0 for _ in range(ACTION_DIM)),
+        train_samples=32,
+        inference_samples=64,
+    )
+
+
+@pytest.fixture()
+def ibc_state() -> IBC:
+    """ダミーの IBC インスタンス(行動-状態)."""
+    return IBC(
+        state_encoder=DummyStateEncoder(),
+        energy_head=DummyHead(),
+        upper_bounds=tuple(1.0 for _ in range(ACTION_DIM)),
+        lower_bounds=tuple(-1.0 for _ in range(ACTION_DIM)),
         train_samples=32,
         inference_samples=64,
     )
@@ -49,10 +63,34 @@ def test__shared_step_timeseries(
     assert loss_dict["loss"].shape == ()
 
 
+def test__shared_step_state(
+    ibc_state: IBC,
+    states: Tensor,
+    actions: Tensor,
+) -> None:
+    """`shared_step` の簡単なテストケース(状態-行動)."""
+    batch = (states, actions)
+    loss_dict = ibc_state.shared_step(batch=batch)
+    assert "loss" in loss_dict
+    assert loss_dict["loss"].shape == ()
+
+
+def test__shared_step_state_timeseries(
+    ibc_state: IBC,
+    states_timeseries: Tensor,
+    actions_timeseries: Tensor,
+) -> None:
+    """`shared_step` の簡単なテストケース(状態-行動)."""
+    batch = (states_timeseries, actions_timeseries)
+    loss_dict = ibc_state.shared_step(batch=batch)
+    assert "loss" in loss_dict
+    assert loss_dict["loss"].shape == ()
+
+
 def test__predict_step(ibc: IBC, observations: Tensor) -> None:
     """`predict_step` の簡単なテストケース."""
     actions = ibc.predict_step(observations)
-    assert actions.shape == (BATCH_SIZE, ACTION_SIZE)
+    assert actions.shape == (BATCH_SIZE, ACTION_DIM)
     assert actions.min() >= -1.0
     assert actions.max() <= 1.0
 
